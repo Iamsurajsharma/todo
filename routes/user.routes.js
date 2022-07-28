1f7dcf79521f9e789229c62c5567f01ffea54c0c
Suraj Sharma <mailforsurajsharma@gmail.com>
2022-07-29 01:09:32 +0530

    Fix User routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
 
-router.get("/users/login", userController.login);
-router.get("/users/sign-up", userController.signUp);
+router.post("/users/login", userController.login);
+router.post("/users/sign-up", userController.signUp);
 
 module.exports = router;
